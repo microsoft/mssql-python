6b9035b6cd227d1f370396a1d88a7d4d3394ea76
Jahnvi Thakkar <jathakkar@microsoft.com>
2024-12-12 09:25:43 +0530

    Making changes for connection function

--- mssql_python/__init__.py
+++ mssql_python/__init__.py
@@ -5,6 +5,12 @@ from .constants import (
 import sys
 from utils import get_odbc_dll_path
 import ctypes
+import logging
+from logging_config import setup_logging
+from exceptions import Exception
+
+# Setting up logging
+setup_logging()
 
 # GLOBALS
 # Read-Only
@@ -38,3 +44,35 @@ elif sys.platform == 'darwin':
     pass
 elif sys.platform == 'linux':
     pass
+
+def connect(connection_str: str) -> Connection:
+    """
+    Constructor for creating a connection to the database.
+
+    Args:
+        connection_str (str): The connection_str to connect to.
+
+    Returns:
+        Connection: A new connection object to interact with the database.
+
+    Raises:
+        DatabaseError: If there is an error while trying to connect to the database.
+        InterfaceError: If there is an error related to the database interface.
+
+    This function provides a way to create a new connection object, which can then
+    be used to perform database operations such as executing queries, committing
+    transactions, and closing the connection.
+    """
+    try:
+        conn = Connection(connection_str)
+        conn._connect_to_db()
+        return conn
+    except DatabaseError as e:
+        logging.error(f"Database error occurred while connecting to the database: {e}")
+        raise DatabaseError(f"Database error occurred while connecting to the database: {e}")
+    except InterfaceError as e:
+        logging.error(f"Interface error occurred while connecting to the database: {e}")
+        raise InterfaceError(f"Interface error occurred while connecting to the database: {e}")
+    except Exception as e:
+        logging.error(f"An error occurred while connecting to the database: {e}")
+        raise Exception(f"An error occurred while connecting to the database: {e}")
--- mssql_python/utils.py
+++ mssql_python/utils.py
@@ -1,8 +1,6 @@
-from connection import Connection 
 import ctypes
 import os
-from constants import ConstantsODBC as odbc_sql_const, ConstantsSQLSTATE as sqlstate
-from exceptions import DatabaseError, InterfaceError
+from constants import ConstantsODBC as odbc_sql_const
 import logging
 from logging_config import setup_logging
 from mssql_python import odbc
@@ -10,38 +8,6 @@ from exceptions import raise_exception, sqlstate_to_exception, Exception
 
 # Setting up logging
 setup_logging()
-
-def connect(connection_str: str) -> Connection:
-        """
-        Constructor for creating a connection to the database.
-
-        Args:
-            connection_str (str): The connection_str to connect to.
-
-        Returns:
-            Connection: A new connection object to interact with the database.
-
-        Raises:
-            DatabaseError: If there is an error while trying to connect to the database.
-            InterfaceError: If there is an error related to the database interface.
-
-        This function provides a way to create a new connection object, which can then
-        be used to perform database operations such as executing queries, committing
-        transactions, and closing the connection.
-        """
-        try:
-            conn = Connection(connection_str)
-            conn._connect_to_db()
-            return conn
-        except DatabaseError as e:
-            logging.error(f"Database error occurred while connecting to the database: {e}")
-            raise DatabaseError(f"Database error occurred while connecting to the database: {e}")
-        except InterfaceError as e:
-            logging.error(f"Interface error occurred while connecting to the database: {e}")
-            raise InterfaceError(f"Interface error occurred while connecting to the database: {e}")
-        except Exception as e:
-            logging.error(f"An error occurred while connecting to the database: {e}")
-            raise Exception(f"An error occurred while connecting to the database: {e}")
 
 def get_odbc_dll_path(dll_name) -> str:
     """
@@ -114,7 +80,7 @@ def add_driver_to_connection_str(connection_str) -> str:
     
     return connection_str     
 
-def check_ret(self, return_code, handle_type, handle) -> None:
+def check_ret(return_code, handle_type, handle) -> None:
     """
     Check the return code from an ODBC function call and handle any errors.
 
