f635691d12c7b521a8e2d011ba3eec515d89604c
Gaurav Sharma <sharmag@microsoft.com>
2024-12-16 16:07:30 +0530

    restructured

---
+++ mssql_python/connection.py
@@ -0,0 +1,111 @@
+from .cursor import Cursor
+
+class Connection:
+    """
+    A class to manage a connection to a database, compliant with DB-API 2.0 specifications.
+
+    This class provides methods to establish a connection to a database, create cursors,
+    commit transactions, roll back transactions, and close the connection. It is designed
+    to be used in a context where database operations are required, such as executing queries
+    and fetching results.
+    
+    Methods:
+        __init__(database: str) -> None:
+        _connect_to_db() -> None:
+        cursor() -> Cursor:
+        commit() -> None:
+        rollback() -> None:
+        close() -> None:
+    """
+  
+    def __init__(self, connection_str: str) -> None:
+        """
+        Initialize the connection object with the specified connection string.
+
+        Args:
+            connection_str (str): The connection_str to connect to.
+
+        Returns:
+            None
+
+        Raises:
+            ValueError: If the connection_str is invalid or connection fails.
+
+        This method sets up the initial state for the connection object, 
+        preparing it for further operations such as connecting to the database, executing queries, etc.
+        """
+        pass
+
+    def _connect_to_db(self) -> None:
+        """
+        Establish a connection to the database.
+
+        This method is responsible for creating a connection to the specified database.
+        It does not take any arguments and does not return any value. The connection
+        details such as database name, user credentials, host, and port should be
+        configured within the class or passed during the class instantiation.
+
+        Raises:
+            DatabaseError: If there is an error while trying to connect to the database.
+            InterfaceError: If there is an error related to the database interface.
+        """
+        pass
+
+    def cursor(self) -> 'Cursor':
+        """
+        Return a new Cursor object using the connection.
+
+        This method creates and returns a new cursor object that can be used to
+        execute SQL queries and fetch results. The cursor is associated with the
+        current connection and allows interaction with the database.
+
+        Returns:
+            Cursor: A new cursor object for executing SQL queries.
+
+        Raises:
+            DatabaseError: If there is an error while creating the cursor.
+            InterfaceError: If there is an error related to the database interface. 
+        """
+        pass
+
+    def commit(self) -> None:
+        """
+        Commit the current transaction.
+        
+        This method commits the current transaction to the database, making all
+        changes made during the transaction permanent. It should be called after
+        executing a series of SQL statements that modify the database to ensure
+        that the changes are saved.
+
+        Raises:
+            DatabaseError: If there is an error while committing the transaction.
+        """
+        pass
+
+    def rollback(self) -> None:
+        """
+        Roll back the current transaction.
+
+        This method rolls back the current transaction, undoing all changes made
+        during the transaction. It should be called if an error occurs during the
+        transaction or if the changes should not be saved.
+
+        Raises:
+            DatabaseError: If there is an error while rolling back the transaction.
+        """
+        pass
+
+    def close(self) -> None:
+        """
+        Close the connection now (rather than whenever .__del__() is called).
+
+        This method closes the connection to the database, releasing any resources
+        associated with it. After calling this method, the connection object should
+        not be used for any further operations. The same applies to all cursor objects
+        trying to use the connection. Note that closing a connection without committing
+        the changes first will cause an implicit rollback to be performed.
+
+        Raises:
+            DatabaseError: If there is an error while closing the connection.
+        """
+        pass
