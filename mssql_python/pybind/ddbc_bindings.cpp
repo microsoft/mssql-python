e146e7e6780627b153d9449842ad5823be55dec2
Gaurav Sharma <sharmag@microsoft.com>
2025-05-13 15:36:59 +0530

    FEAT: Entra ID support with `mssql-auth` inside package
    * FEAT: Entra ID support using mssql-auth
    
    * resolved comments

--- mssql_python/pybind/ddbc_bindings.cpp
+++ mssql_python/pybind/ddbc_bindings.cpp
@@ -246,6 +246,16 @@ void LoadDriverOrThrowException() {
         ThrowStdException("Failed to load driver");
     }
 
+    // Preload mssql-auth.dll from the same path if available
+    // TODO: Only load mssql-auth.dll if using Entra ID Authentication modes (Active Directory modes)
+    std::wstring authDllDir = std::wstring(ddbcModulePath) + L"\\libs\\win\\mssql-auth.dll";
+    HMODULE hAuthModule = LoadLibraryW(authDllDir.c_str());
+    if (hAuthModule) {
+        LOG("Authentication library loaded successfully from - {}", authDllDir.c_str());
+    } else {
+        LOG("Note: Authentication library not found at - {}. This is OK if you're not using Entra ID Authentication.", authDllDir.c_str());
+    }
+
     // Look for msodbcsql18.dll in a path relative to DDBC module
     std::wstring dllDir = std::wstring(ddbcModulePath) + L"\\libs\\win\\msodbcsql18.dll";
     HMODULE hModule = LoadLibraryW(dllDir.c_str());
