28938dfcc090129216db4b365800835a8ec2a3f6
Gaurav Sharma <sharmag@microsoft.com>
2025-07-16 10:56:01 +0530

    restored main

--- main.py
+++ main.py
@@ -1,32 +1,12 @@
+from mssql_python import connect
+from mssql_python import setup_logging
 import os
 import decimal
 
-
-conn_str = os.getenv("DB_CONNECTION_STRING")
-# print the connection string without pwd
-if conn_str:
-    secure_connection_str = ''
-    for i in conn_str.split(';'):
-        if i.lower().startswith('pwd='):
-            secure_connection_str += 'pwd=******;'
-        else:
-            secure_connection_str += i + ';'
-    print(f"Connecting to database with connection string: {secure_connection_str}")
-
-from mssql_python import connect
-from mssql_python import setup_logging
 setup_logging('stdout')
 
-try:
-    conn = connect(conn_str)
-except Exception as e:
-    if 'Timeout' in str(e):
-        print("retrying in 60 seconds...")
-        import time
-        time.sleep(60)
-        conn = connect(conn_str)
-    else:
-        raise e
+conn_str = os.getenv("DB_CONNECTION_STRING")
+conn = connect(conn_str)
 
 # conn.autocommit = True
 
