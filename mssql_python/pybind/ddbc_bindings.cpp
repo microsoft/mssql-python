94237da45310411394a1e74b1bf425668da52d00
Gaurav Sharma <sharmag@microsoft.com>
2025-09-08 12:28:29 +0530

    reversing unwanted changes

--- mssql_python/pybind/ddbc_bindings.cpp
+++ mssql_python/pybind/ddbc_bindings.cpp
@@ -1369,12 +1369,6 @@ SQLRETURN BindParameterArray(SQLHANDLE hStmt,
                                 std::string offending = WideToUTF8(wstr);
                                 ThrowStdException("Input string exceeds allowed column size at parameter index " + std::to_string(paramIndex));
                             }
-#if defined(__APPLE__) || defined(__linux__)
-                            auto utf16Buf = WStringToSQLWCHAR(wstr);
-                            size_t copySize = std::min(utf16Buf.size(), static_cast<size_t>(info.columnSize + 1));
-                            std::memcpy(wcharArray + i * (info.columnSize + 1), utf16Buf.data(), copySize * sizeof(SQLWCHAR));
-#else
-                            std::memcpy(wcharArray + i * (info.columnSize + 1), wstr.c_str(), (wstr.length() + 1) * sizeof(SQLWCHAR));
 #endif
                             strLenOrIndArray[i] = SQL_NTS;
                         }
