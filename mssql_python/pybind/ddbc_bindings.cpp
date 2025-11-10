7ad094789c877a3e3d31ab8230637006310dfbf7
Gaurav Sharma <sharmag@microsoft.com>
2025-11-10 16:34:34 +0530

    OPTIMIZATION #3 (FIX): Remove unused columnSize variables (Windows build fix)
    Windows compiler treats warnings as errors (/WX flag). The columnSize
    variable was extracted from columnSizes array but never used in the
    SQL_CHAR and SQL_WCHAR cases after OPTIMIZATION #3.
    
    Changes:
    --------
    - Removed unused 'SQLULEN columnSize' declaration from SQL_CHAR/VARCHAR/LONGVARCHAR case
    - Removed unused 'SQLULEN columnSize' declaration from SQL_WCHAR/WVARCHAR/WLONGVARCHAR case
    - Retained fetchBufferSize and isLob which are actually used
    
    This fixes Windows build errors:
    - error C2220: warning treated as error
    - warning C4189: 'columnSize': local variable is initialized but not referenced
    
    The optimization remains intact - metadata is still prefetched from cache-friendly arrays.

--- mssql_python/pybind/ddbc_bindings.cpp
+++ mssql_python/pybind/ddbc_bindings.cpp
@@ -3280,7 +3280,6 @@ SQLRETURN FetchBatchData(SQLHSTMT hStmt, ColumnBuffers& buffers, py::list& colum
                 case SQL_CHAR:
                 case SQL_VARCHAR:
                 case SQL_LONGVARCHAR: {
-                    SQLULEN columnSize = columnSizes[col - 1];
                     uint64_t fetchBufferSize = fetchBufferSizes[col - 1];
 					uint64_t numCharsInData = dataLen / sizeof(SQLCHAR);
                     bool isLob = isLobs[col - 1];
@@ -3298,7 +3297,6 @@ SQLRETURN FetchBatchData(SQLHSTMT hStmt, ColumnBuffers& buffers, py::list& colum
                 case SQL_WVARCHAR:
                 case SQL_WLONGVARCHAR: {
                     // TODO: variable length data needs special handling, this logic wont suffice
-                    SQLULEN columnSize = columnSizes[col - 1];
                     uint64_t fetchBufferSize = fetchBufferSizes[col - 1];
 					uint64_t numCharsInData = dataLen / sizeof(SQLWCHAR);
                     bool isLob = isLobs[col - 1];
